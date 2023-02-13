27c3cd92de375ec124eaf7e509d54fc7c4501071
George <gnjuki19@gmail.com>
2023-02-13 12:13:52 +0300

    Rendered the app before login and signup, all links working and app responsive on mobile and tablet too.

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,20 +1,43 @@
-import '../App.css';
-import NavBar from './NavBar';
-import Home from './Home';
-import LoginForm from './LoginForm';
-import AlbumsPage from './AlbumsPage';
-import PhotosPage from './PhotosPage';
-import SignupForm from './SignupForm';
+import { useState, useEffect } from "react";
+import "../App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import LoginForm from "./LoginForm";
+import AlbumsPage from "./AlbumsPage";
+import PhotosPage from "./PhotosPage";
+import SignupForm from "./SignupForm";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("/me").then((res) => {
+      if (res.ok) {
+        res.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
-      <NavBar />
-      <Home />
-      <AlbumsPage />
-      <PhotosPage />
-      <LoginForm />
-      <SignupForm />
+      <NavBar user={user} setUser={setUser} />
+      <main>
+        {user ? (
+          <Routes>
+            <Route exact path="/" element={<Home user={user} />} />
+            <Route path="/albums" element={<AlbumsPage user={user} />} />
+            <Route path="/photos" element={<PhotosPage user={user} />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/login" element={<LoginForm setUser={setUser} />} />
+            <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        )}
+      </main>
+      {/* <Footer /> */}
     </div>
   );
 }

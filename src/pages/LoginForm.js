882a51c22b132964bb8c23174896bc2b61f276cf
George <gnjuki19@gmail.com>
2023-02-14 17:19:09 +0300

    Trial and error tofix CORS issue

--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -11,6 +11,7 @@ function LoginForm({ setUser }) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({ username, password }),
     }).then((res) => {
